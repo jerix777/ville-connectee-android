e359ad071d3353fa6a951c41465069cb98028655
Jerix777 <kouameyaojeremie@gmail.com>
2025-07-09 10:31:54 +0000

    chore: update Capacitor dependencies to version 7.4.1

---
+++ android/app/src/main/java/com/villeconnectee/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.villeconnectee.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.villeconnectee.app',
+  appName: 'Ville Connectée',
+  webDir: 'dist'
+};
+
+export default config;
